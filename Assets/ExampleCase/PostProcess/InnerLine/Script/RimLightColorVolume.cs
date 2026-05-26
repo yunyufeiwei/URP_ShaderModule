4209814a5e6b99fbbef0776b305e5c33b0e14f87
staly_xiaoyu <54066197+yunyufeiwei@users.noreply.github.com>
2026-05-26 23:42:08 +0800

    更新

---
+++ Assets/ExampleCase/PostProcess/InnerLine/Script/RimLightColorVolume.cs
@@ -0,0 +1,18 @@
+using System;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+[ImageEffectAllowedInSceneView]
+[Serializable, VolumeComponentMenu("CustomPostProcessing/RimLightColor")]
+public class RimLightColorVolume : VolumeComponent
+{
+    public FloatParameter thicknessX = new FloatParameter(0.01f , true);
+    public FloatParameter thicknessY = new FloatParameter(0.01f , true);
+    public ClampedFloatParameter maxThickness = new ClampedFloatParameter(0.01f, 0.0f, 0.5f);
+    public FloatParameter intensity = new FloatParameter(1 , true); 
+    public ClampedFloatParameter lerpValue = new ClampedFloatParameter(1.0f , 0.0f , 1.0f);
+    public FloatParameter distance = new FloatParameter(0.01f , true);
+    public ColorParameter rimLightColor = new ColorParameter(Color.white, true);
+    
+    public LayerMaskParameter layerMask = new LayerMaskParameter(-1); // -1 means all layers by default
+}
